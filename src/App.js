fb161903642b287bd05ed4a9f98f36ee609a111c
Luis Cañate <canatel@uninorte.edu.co>
2022-05-19 22:01:48 -0500

    Login and Signup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from "./Components/ProtectedRoute";
+import { UserProvider } from "./Components/UserProvider";
+
+import Header from "./Components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import SignUpScreen from "./Screens/SignUpScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomeScreen />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/signup" element={<SignUpScreen />} />
+        <Route path="*" element={<LoginScreen />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
